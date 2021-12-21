87ab46068d9c7942f9272849ece7918b260a53d9
Tiago Onofre <taraujonofre@gmail.com>
2021-12-20 22:02:19 -0300

    Generated -> converted to Sexp -> encoded to base 64

--- DSAExample/_build/default/cr.ml
+++ DSAExample/_build/default/cr.ml
@@ -2,17 +2,20 @@ open Mirage_crypto_pk
 
 let () = Mirage_crypto_rng_unix.initialize ()
 
-let private_key = Dsa.generate `Fips2048
+let private_key = Dsa.generate `Fips1024
 
 let public_key = Dsa.pub_of_priv private_key
 
 let str = Z.to_string public_key.y
 let str2 = Z.to_string private_key.x
 
-(* or_digest is a type not a function -> need to know how to represent 
- * the pub key or know if i really need this *)
-let str_private_key = Cstruct.to_string (Cstruct.create 89)
+let str_priv_key = Sexplib0.Sexp.to_string_hum ~indent:64 (Dsa.sexp_of_priv private_key)
 
-let () = Printf.printf "Public string: %s\nPrivate String: %s\n%!" str str2
+let encoded_priv_key = Base64.encode_string str_priv_key 
+
+(* Current problem -> How to represent the keys with a string? *)
+(*let str_private_key = Cstruct.to_string (Cstruct.create 89)*)
+
+let () = Printf.printf "Public string: %s\nPrivate String: %s\n%!" str encoded_priv_key
 
 
--- DSAExample/cr.ml
+++ DSAExample/cr.ml
@@ -2,16 +2,20 @@ open Mirage_crypto_pk
 
 let () = Mirage_crypto_rng_unix.initialize ()
 
-let private_key = Dsa.generate `Fips2048
+let private_key = Dsa.generate `Fips1024
 
 let public_key = Dsa.pub_of_priv private_key
 
 let str = Z.to_string public_key.y
 let str2 = Z.to_string private_key.x
 
+let str_priv_key = Sexplib0.Sexp.to_string_hum ~indent:64 (Dsa.sexp_of_priv private_key)
+
+let encoded_priv_key = Base64.encode_string str_priv_key 
+
 (* Current problem -> How to represent the keys with a string? *)
 (*let str_private_key = Cstruct.to_string (Cstruct.create 89)*)
 
-let () = Printf.printf "Public string: %s\nPrivate String: %s\n%!" str str2
+let () = Printf.printf "Public string: %s\nPrivate String: %s\n%!" str encoded_priv_key
 
 
