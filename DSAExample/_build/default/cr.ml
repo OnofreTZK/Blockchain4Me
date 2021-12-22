f5de5c3c0a7bf029a97a2ffebf5e5a578b305ed0
Tiago Onofre <taraujonofre@gmail.com>
2021-12-21 21:41:40 -0300

    Signature and convert to string

--- DSAExample/_build/default/cr.ml
+++ DSAExample/_build/default/cr.ml
@@ -9,13 +9,25 @@ let public_key = Dsa.pub_of_priv private_key
 let str = Z.to_string public_key.y
 let str2 = Z.to_string private_key.x
 
-let str_priv_key = Sexplib0.Sexp.to_string_hum ~indent:64 (Dsa.sexp_of_priv private_key)
+let str_priv_key = Sexplib0.Sexp.to_string (Dsa.sexp_of_priv private_key)
 
 let encoded_priv_key = Base64.encode_string str_priv_key 
 
-(* Current problem -> How to represent the keys with a string? *)
 (*let str_private_key = Cstruct.to_string (Cstruct.create 89)*)
 
 let () = Printf.printf "Public string: %s\nPrivate String: %s\n%!" str encoded_priv_key
+
+let signature = Dsa.sign ~key:private_key (Cstruct.of_string "Message to sign")
+
+let encoded_signature = 
+  let signature_str = signature |> fun (r, s) -> (Cstruct.to_string r) ^ " " ^ (Cstruct.to_string s)
+  in
+  Base64.encode_string signature_str
+
+let () = Printf.printf "Signature string: %s\n%!" encoded_signature
+
+
+
+
 
 
